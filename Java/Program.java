17b25e5fa2e54946e8985e4800ece3f738dd956b
De <spark49942@gmail.com>
2023-01-26 04:29:02 +0300

    ZD

---
+++ Java/Program.java
@@ -0,0 +1,15 @@
+package Java;
+import java.util.*;
+
+public class Program
+{
+    public static void main(String[] args) 
+    {
+        Scanner work = new Scanner(System.in);
+        System.out.println("Введите желаемый параметр поиска (название\\год выпуска\\режиссёр): ");
+        String myName = work.nextLine();
+        new Filmname(myName);
+        work.close();
+        System.out.println();
+    }
+}
---
+++ Program.py
@@ -0,0 +1,4 @@
+from Filmname import Filmname
+
+myName = input("Введите желаемый параметр поиска (название\\год выпуска\\режиссёр): ")
+search = Filmname(myName)
